343ee088a128d2a78b2f88be518ada2b833b0650
ljn <liujinnan@idealsee.cn>
2017-12-11 18:41:42 +0800

    提交修改

--- Assets/plyoung/assembly/editor/BloxEditor.Databinding/ComparisonCheckEd.cs
+++ Assets/plyoung/assembly/editor/BloxEditor.Databinding/ComparisonCheckEd.cs
@@ -38,38 +38,38 @@ namespace BloxEditor.Databinding
 
 		protected override void Draw(Rect rect, DataProvider target, bool isSetter)
 		{
-			ComparisonDataProvider comparisonDataProvider = target as ComparisonDataProvider;
-			rect.height = EditorGUIUtility.singleLineHeight;
-			if (ComparisonCheckEd.GC_Operator == null)
-			{
-				ComparisonCheckEd.GC_Operator = new GUIContent[ComparisonCheckEd.S_ComparisonOpt.Length];
-				for (int i = 0; i < ComparisonCheckEd.GC_Operator.Length; i++)
-				{
-					ComparisonCheckEd.GC_Operator[i] = new GUIContent(ComparisonCheckEd.S_ComparisonOpt[i]);
-				}
-			}
-			Rect r = rect;
-			Rect position = rect;
-			position.width = 28f;
-			r.width = (float)((r.width - position.width) / 2.0);
-			position.x += r.width;
-			DataProviderEd.DataBindingField(r, null, comparisonDataProvider.param1, comparisonDataProvider, false, null);
-			if (GUI.Button(position, ComparisonCheckEd.GC_Operator[(int)comparisonDataProvider.comparisonOpt]))
-			{
-				ComparisonCheckEd.optMenu = new GenericMenu();
-				for (int j = 0; j < ComparisonCheckEd.GC_Operator.Length; j++)
-				{
-					ComparisonCheckEd.optMenu.AddItem(ComparisonCheckEd.GC_Operator[j], false, this.OnOptChosen, new Data
-					{
-						obj = comparisonDataProvider,
-						idx = j
-					});
-				}
-				ComparisonCheckEd.optMenu.DropDown(position);
-			}
-			r.x = r.x + r.width + position.width;
-			DataProviderEd.DataBindingField(r, null, comparisonDataProvider.param2, comparisonDataProvider, false, null);
-		}
+            ComparisonDataProvider comparisonDataProvider = target as ComparisonDataProvider;
+            rect.height = EditorGUIUtility.singleLineHeight;
+            if (ComparisonCheckEd.GC_Operator == null)
+            {
+                ComparisonCheckEd.GC_Operator = new GUIContent[ComparisonCheckEd.S_ComparisonOpt.Length];
+                for (int i = 0; i < ComparisonCheckEd.GC_Operator.Length; i++)
+                {
+                    ComparisonCheckEd.GC_Operator[i] = new GUIContent(ComparisonCheckEd.S_ComparisonOpt[i]);
+                }
+            }
+            Rect r = rect;
+            Rect position = rect;
+            position.width = 28f;
+            r.width = (float)((r.width - position.width) / 2.0);
+            position.x += r.width;
+            DataProviderEd.DataBindingField(r, null, comparisonDataProvider.param1, comparisonDataProvider, false, null);
+            if (GUI.Button(position, ComparisonCheckEd.GC_Operator[(int)comparisonDataProvider.comparisonOpt]))
+            {
+                ComparisonCheckEd.optMenu = new GenericMenu();
+                for (int j = 0; j < ComparisonCheckEd.GC_Operator.Length; j++)
+                {
+                    ComparisonCheckEd.optMenu.AddItem(ComparisonCheckEd.GC_Operator[j], false, this.OnOptChosen, new Data
+                    {
+                        obj = comparisonDataProvider,
+                        idx = j
+                    });
+                }
+                ComparisonCheckEd.optMenu.DropDown(position);
+            }
+            r.x = r.x + r.width + position.width;
+            DataProviderEd.DataBindingField(r, null, comparisonDataProvider.param2, comparisonDataProvider, false, null);
+        }
 
 		private void OnOptChosen(object arg)
 		{
